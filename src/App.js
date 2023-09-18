3658d567d5ddca06825582190a236dd64aa1b0bd
Nathan Nguyendinh <nathannguyendinh1030@yahoo.com>
2023-09-18 02:12:09 -0700

    Basic Footer and Fonts implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./styles.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <div className="App">
       <Navbar/>
+      HEY!
+      <Footer/>
     </div>
   );
 }
