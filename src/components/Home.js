7f0b230ea34a944e7a24eb512ee8ff6b78dcd6d7
Nathan Nguyendinh <30915871+nnguyendinh@users.noreply.github.com>
2023-09-18 21:41:36 -0700

    Added Image and Spacing for Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Component } from 'react';
 import "../styles/HomeStyles.css";
 
+import teaching from "../images/teaching.jpg";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
@@ -16,9 +17,28 @@ class Home extends Component {
             <div className="about">
                 <h2> Nathan Nguyendinh</h2>
                 <div className="description">
-                    <p>The one thing I value most in life is learning. Whether it be learning new things myself, or helping others learn something, I find no satisfaction greater than watching the brain’s gears begin to turn. I am lucky to have had so many opportunities to gain new knowledge and to spread that knowledge, and I hope to continue doing both far into the future. <br/> <br/>
-                    Right now, I currently study Computer Engineering at the University of California, Los Angeles, where I focus on embedded systems, RTL design, signal processing, and whatever else piques my interest. My end goal is to keep expanding my knowledge in new areas and eventually become the jack of all trades within the digital field. I am always looking for new ways to apply what I learn, so please don’t hesitate to connect with me!
+                    <p>The one thing I value most in life is learning. 
+                        Whether it be learning new things myself, or 
+                        helping others learn something, I find no 
+                        satisfaction greater than watching the brain’s 
+                        gears begin to turn. I am lucky to have had so 
+                        many opportunities to gain new knowledge and 
+                        to spread that knowledge, and I hope to continue 
+                        doing both far into the future. <br/> <br/>
+
+                        Right now, I currently study Computer Engineering 
+                        at the University of California, Los Angeles, where 
+                        I focus on embedded systems, RTL design, signal 
+                        processing, and whatever else piques my interest. 
+                        My end goal is to keep expanding my knowledge in 
+                        new areas and eventually become the jack of all 
+                        trades within the digital field. I am always 
+                        looking for new ways to apply what I learn, so 
+                        please don’t hesitate to connect with me!
                     </p>
+                    <img src={teaching} alt="Teaching" className="image"/>
+                </div>
+                <div className="socials">
                     <a href="https://github.com/nnguyendinh">
                         <GitHubIcon/>   
                     </a>
