d8b58616329966191969aa94f8872ea8ee7fa897
Nathan Nguyendinh <30915871+nnguyendinh@users.noreply.github.com>
2023-09-19 22:18:04 -0700

    Changed all but navbar to functional components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,37 +1,31 @@
 import React from "react";
-import { Component } from 'react';
-
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
 import DescriptionIcon from '@mui/icons-material/Description';
 import "../styles/FooterStyles.css";
 
+function Footer () {
 
-class Footer extends Component {
-
-    render() 
-    {
-      return (
-        <div className="footer"> 
-            <div className="socials">
-                <a href="https://github.com/nnguyendinh">
-                    <GitHubIcon/>
-                </a>
-                <a href="https://www.linkedin.com/in/nathan-nguyendinh/">
-                    <LinkedInIcon/>
-                </a>
-                <a href="mailto:nnguyendinh@g.ucla.edu">
-                    <EmailIcon/>
-                </a>
-                <a href="https://github.com/nnguyendinh">
-                    <DescriptionIcon/>
-                </a>
-            </div>
-        <p> &copy; 2023 nnguyendinh.com</p>
+    return (
+    <div className="footer"> 
+        <div className="socials">
+            <a href="https://github.com/nnguyendinh">
+                <GitHubIcon/>
+            </a>
+            <a href="https://www.linkedin.com/in/nathan-nguyendinh/">
+                <LinkedInIcon/>
+            </a>
+            <a href="mailto:nnguyendinh@g.ucla.edu">
+                <EmailIcon/>
+            </a>
+            <a href="https://github.com/nnguyendinh">
+                <DescriptionIcon/>
+            </a>
         </div>
-      );
-    }
-  }
+    <p> &copy; 2023 nnguyendinh.com</p>
+    </div>
+    );
+}
   
   export default Footer
