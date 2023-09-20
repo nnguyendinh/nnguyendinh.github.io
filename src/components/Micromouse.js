427a1343b02fa2cf7ad88ee3294d87c9c2ae6198
Nathan Nguyendinh <30915871+nnguyendinh@users.noreply.github.com>
2023-09-19 22:07:00 -0700

    setup navigation to individual micromouse page

---
+++ src/components/Micromouse.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Projects from './Projects';
+import BackToMain from './BackToMain';
+
+function Micromouse() {
+  return (
+    <div>
+      <BackToMain/>
+      <Projects/>
+    </div>
+  );
+}
+
+export default Micromouse;
