d4da7e352c42a936d4027609d9df422bee49f6fc
Nathan Nguyendinh <30915871+nnguyendinh@users.noreply.github.com>
2023-09-26 23:55:02 -0700

    Added project title pictures

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,18 +3,27 @@ import ProjectItem from "./ProjectItem";
 import "../styles/ProjectsStyles.css";
 
 import proj1 from "../images/proj1.jpg";
+import banner from "../images/banner.png";
+import logo from "../images/logo.png";
+import OOP from "../images/OOP_crop.png";
+import guitar from "../images/guitar_crop.png";
+import dav from "../images/dav.png";
+import dav1 from "../images/dav1_crop.png";
+import mm from "../images/MM_crop.jpg";
+import rt from "../images/realtime_crop.jpg"
+
 
 function Projects() {
     return (
         <div className="projects">
         <h1 className="projectHeader"> Projects</h1>
             <div className="projectList">
-                <ProjectItem image={proj1} name="Micromouse" id="micromouse"/>
-                <ProjectItem image={proj1} name="Digital Audio Visualizer" id="digital-audio-visualizer"/>
-                <ProjectItem image={proj1} name="RISC-V Out of Order Processor" id="risc-v-processor"/>
-                <ProjectItem image={proj1} name="Bruin Bins" id="bruin-bins"/>
-                <ProjectItem image={proj1} name="FPGA Guitar Hero" id="guitar-hero"/>
-                <ProjectItem image={proj1} name="Realtime Instrument Detector" id="instrument-detector"/>
+                <ProjectItem image={mm} name="Micromouse" id="micromouse"/>
+                <ProjectItem image={guitar} name="FPGA Guitar Hero" id="guitar-hero"/>
+                <ProjectItem image={OOP} name="RISC-V Out of Order Processor" id="risc-v-processor"/>
+                <ProjectItem image={dav1} name="Digital Audio Visualizer" id="digital-audio-visualizer"/>
+                <ProjectItem image={banner} name="Bruin Bins" id="bruin-bins"/>
+                <ProjectItem image={rt} name="Realtime Instrument Detector" id="instrument-detector"/>
             </div>
         </div>
     );
