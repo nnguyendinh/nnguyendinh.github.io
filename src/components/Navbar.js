582260b935c4d4d6726bc7044e5ffae1d85ddc54
Nathan Nguyendinh <30915871+nnguyendinh@users.noreply.github.com>
2023-09-27 01:29:55 -0700

    Replaced the navbar icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Component } from 'react'
 import { Link } from 'react-scroll';
 import '../styles/NavbarStyles.css'
+import CottageIcon from '@mui/icons-material/Cottage';
 
 class Navbar extends Component {
   state = { clicked: false };
@@ -13,7 +14,7 @@ class Navbar extends Component {
     return (
       <nav>
         <a href="/">
-          <svg id="logo-35" width="50" height="39" viewBox="0 0 50 39" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M16.4992 2H37.5808L22.0816 24.9729H1L16.4992 2Z" class="ccompli1" fill="#007AFF"></path> <path d="M17.4224 27.102L11.4192 36H33.5008L49 13.0271H32.7024L23.2064 27.102H17.4224Z" class="ccustom" fill="#312ECB"></path> </svg>
+          <CottageIcon fontSize='large'/>
         </a>
         <div>
           <ul id="navbar" className={this.state.clicked ? "#navbar active" : "navbar"}>
